6a83f646f7ab0c5cf3378919dee40b9c67a90bb9
Rabin <53074799+Rabin321@users.noreply.github.com>
2022-02-04 21:19:30 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+//messenger button show/hide script
+        if(this.scrollY > 500){
+            $('.messenger').addClass("show");
+        }else{
+            $('.messenger').removeClass("show");
+        }
+    });
+
+  
+
+
+  
+});
